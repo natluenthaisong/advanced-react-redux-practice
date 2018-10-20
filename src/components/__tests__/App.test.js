491c07df4e8e2e54fbc4c12d415d2252c7aed265
Nattavut Luenthaisong <nattavut.luenthaisong@motili.com>
2018-10-20 22:05:48 +0700

    add .env for NODE_PATH to absolute

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-   const wrapped = shallow(<App />);
 
    expect(wrapped.find(CommentBox).length).toEqual(1)
+});
+
+it('shows a comment list', () => {
+
+    expect(wrapped.find(CommentList).length).toEqual(1)
 });
