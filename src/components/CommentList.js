c317836286bab48e916c4e5c80086a755fc97ef9
Nattavut Luenthaisong <nattavut.luenthaisong@motili.com>
2018-10-22 12:46:30 +0700

    update test case for CommentList component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class CommentList extends Component {
+    renderComments() {
+        return this.props.comments.map(comment => {
+            return <li key={comment}>{comment}</li>;
+        })
+    }
+
     render() {
         return (
-            <div>Comment List</div>
+            <div>
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
         );
     }
 }
 
-export default CommentList;
+function mapStateToProps(state) {
+    return { comments: state.comments};
+}
+
+export default connect(mapStateToProps)(CommentList);
